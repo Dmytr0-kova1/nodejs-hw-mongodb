093923e077dd769db074b29203967556cfc84cdd
Dmytro Koval <kdmytro793@gmail.com>
2025-03-17 21:38:31 +0200

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,17 +7,32 @@ import {
   deletContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../validation/validateBody.js';
+import {
+  createContactShema,
+  updateContactShema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post(
+  '/',
+  validateBody(createContactShema),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactShema),
+  ctrlWrapper(patchContactController),
+);
 
-router.delete('/:contactId', ctrlWrapper(deletContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deletContactController));
 
 export default router;
