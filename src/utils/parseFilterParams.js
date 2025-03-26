30d3c6e4a7cbb4b179dbb32e43c7ff6a2bc78d3e
Dmytro Koval <kdmytro793@gmail.com>
2025-03-26 22:15:25 +0200

    added filter

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,30 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+
+  if (isContactType(contactType)) return contactType;
+};
+
+const parseBoolean = (isFavourite) => {
+  const isString = typeof isFavourite === 'string';
+  if (!isString) return;
+
+  if (isFavourite === 'true') true;
+  if (isFavourite === 'false') false;
+
+  return isFavourite;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedContactType = parseContactType(contactType);
+  const parsedIsFavourite = parseBoolean(isFavourite);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
